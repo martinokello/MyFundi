920735a7e9c195f76b4961c6d6a204ab3c294757
Martin Alex Okello <martin.okello@gmail.com>
2022-07-20 13:24:41 +0100

    Introduction to JobWorkCategory Entity, to reduce via link Table to 3rd Normal Form

---
+++ MyFundi.Services/RepositoryServices/Concretes/JobWorkCategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using MyFundi.Domain;
+using MyFundi.Services.RepositoryServices.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyFundi.Services.RepositoryServices.Concretes
+{
+   public class JobWorkCategoryRepository: AbstractRepository<JobWorkCategory>
+    {
+        public override bool Delete(JobWorkCategory toDelete)
+        {
+            try
+            {
+                toDelete = GetById(toDelete.JobWorkCategoryId);
+                MyFundiDBContext.Remove(toDelete);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public override JobWorkCategory GetByGuid(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override JobWorkCategory GetById(int id)
+        {
+            return MyFundiDBContext.JobWorkCategories.SingleOrDefault(p => p.JobWorkCategoryId == id);
+        }
+
+        public override bool Update(JobWorkCategory toUpdate)
+        {
+            try
+            {
+                var item = GetById(toUpdate.JobWorkCategoryId);
+
+                item.JobId = toUpdate.JobId;
+                item.WorkCategoryId = toUpdate.WorkCategoryId;
+                item.DateUpdated = DateTime.Now;
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
